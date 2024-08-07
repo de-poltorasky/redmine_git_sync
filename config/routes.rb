0ed8b56281c83d84578b69858960d2429126c177
de-poltorasky <88427031+de-poltorasky@users.noreply.github.com>
2024-08-07 12:05:08 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ resources :projects do
     collection do
       post 'sync'
     end
-    member do
-      get 'show_repo', to: 'git_sync#show', as: 'show_repo'
-      get 'view_file', to: 'git_sync#view_file', as: 'view_file'
-    end
   end
 end
 
