83576926bdd11a21b1b6c2eb2006adaf9fbdcf6f
de-poltorasky <88427031+de-poltorasky@users.noreply.github.com>
2024-08-07 11:52:45 +0200

    Update git_sync_controller.rb

--- app/controllers/git_sync_controller.rb
+++ app/controllers/git_sync_controller.rb
@@ -21,30 +21,28 @@ class GitSyncController < ApplicationController
     api_key = params[:api_key]
     project_name = params[:project_name]
     skip_ssl_verification = params[:skip_ssl_verification] == '1'
-    timestamp = Time.now.strftime('%Y%m%d%H%M%S')
-    project_dir = File.join(Rails.root, 'plugins', 'redmine_git_sync', 'repositories', project_name)
-
-    # Delete project directory if it exists
-    if Dir.exists?(project_dir)
-      FileUtils.rm_rf(project_dir)
-    end
-
-    # Create new project directory
-    FileUtils.mkdir_p(project_dir)
+    project_dir = File.join(Rails.root, 'plugins', 'redmine_git_sync', 'repositories', "#{project_name}_#{@project.id}")
 
     # Formatted clone URL
     clone_url = "https://#{api_key}@#{source_repo.sub(/^https:\/\//, '')}"
 
-    # Git clone command
-    git_clone_cmd = "git clone #{'--config http.sslVerify=false ' if skip_ssl_verification}#{clone_url} #{project_dir}"
-    git_push_cmd = "cd #{project_dir} && git push --mirror ."
+    if Dir.exists?(project_dir)
+      # Update existing repository
+      git_pull_cmd = "cd #{project_dir} && git pull #{'--config http.sslVerify=false ' if skip_ssl_verification}#{clone_url}"
+      success = system(git_pull_cmd)
+    else
+      # Clone new repository
+      FileUtils.mkdir_p(project_dir)
+      git_clone_cmd = "git clone #{'--config http.sslVerify=false ' if skip_ssl_verification}#{clone_url} #{project_dir}"
+      success = system(git_clone_cmd)
+    end
 
-    if system(git_clone_cmd) && system(git_push_cmd)
+    if success
       flash[:notice] = 'Synchronization successful'
     else
       flash[:error] = 'Synchronization failed'
     end
-    redirect_to project_git_sync_index_path(@project)
+    redirect_to project_git_sync_index_path(@project.id)
   end
 
   private
