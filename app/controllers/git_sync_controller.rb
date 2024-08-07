2686472b2c3ff97b74a8832a822e530ab2215214
de-poltorasky <88427031+de-poltorasky@users.noreply.github.com>
2024-08-07 10:19:38 +0200

    Update git_sync_controller.rb

--- app/controllers/git_sync_controller.rb
+++ app/controllers/git_sync_controller.rb
@@ -9,15 +9,21 @@ class GitSyncController < ApplicationController
     source_repo = params[:source_repo]
     api_key = params[:api_key]
     project_name = params[:project_name]
+    skip_ssl_verification = params[:skip_ssl_verification] == '1'
     timestamp = Time.now.strftime('%Y%m%d%H%M%S')
     project_dir = File.join(Rails.root, 'plugins', 'redmine_git_sync', 'repositories', "#{project_name}_#{timestamp}")
 
     # Create project directory if it doesn't exist
     FileUtils.mkdir_p(project_dir) unless Dir.exists?(project_dir)
 
-    # Clone and push using API key
-    clone_url = "https://#{api_key}@#{source_repo}"
-    if system("git clone #{clone_url} #{project_dir} && cd #{project_dir} && git push --mirror .")
+    # Formatted clone URL
+    clone_url = "https://#{api_key}@#{source_repo.sub(/^https:\/\//, '')}"
+
+    # Git clone command
+    git_clone_cmd = "git clone #{'--config http.sslVerify=false ' if skip_ssl_verification}#{clone_url} #{project_dir}"
+    git_push_cmd = "cd #{project_dir} && git push --mirror ."
+
+    if system(git_clone_cmd) && system(git_push_cmd)
       flash[:notice] = 'Synchronization successful'
     else
       flash[:error] = 'Synchronization failed'
